fcf1ed2960297afb37e6a946980c6b2c4143a6e0
vipulbhj <vipulsworkmail@gmail.com>
2019-10-31 17:51:50 +0530

    fixed review changes

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,9 @@
 export default class Utils {
+  /**
+   * extension helper:- function takes url as and input and return url after removing query parameteres.
+   *
+   * @memberof Utils
+   */
   urlWithoutQueryParameters(url) {
     let u = new URL(url);
     return u.origin + u.pathname;
